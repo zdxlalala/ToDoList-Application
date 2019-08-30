c8845a1e5a1459a773303deeb3e6b800dfe58568
zdxlalala <52614721+zdxlalala@users.noreply.github.com>
2019-08-29 22:06:35 -0700

    Helper class

---
+++ main/src/model/Helper.java
@@ -0,0 +1,17 @@
+package model;
+
+
+public class Helper {
+
+    //EFFECTS: return true if two int are equal, false other vise
+    public Boolean equal(int x, int y) {
+        return x == y;
+    }
+
+    //REQUIRES: i > 0
+    //EFFECTS: return the given int minus one
+    public int minusOne(int i) {
+        return i - 1;
+    }
+
+}
