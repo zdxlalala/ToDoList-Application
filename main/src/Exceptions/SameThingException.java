a3b59f5f76f0c4d456b50362dc9b0e10d3d7bb4a
zdxlalala <52614721+zdxlalala@users.noreply.github.com>
2019-08-29 22:01:36 -0700

    P7/8

---
+++ main/src/Exceptions/SameThingException.java
@@ -0,0 +1,4 @@
+package model.Exceptions;
+
+public class SameThingException extends Exception{
+}
---
+++ main/src/Exceptions/TooManyThingsToDoException.java
@@ -0,0 +1,4 @@
+package model.Exceptions;
+
+public class TooManyThingsToDoException extends Exception{
+}
